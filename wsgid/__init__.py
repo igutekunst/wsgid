3eaa470fd0e15432d46d3f4878898bf46f3d1f5e
Dalton Barreto <daltonmatos@gmail.com>
2012-02-27 14:38:05 -0300

    General pep8 adjustments

--- wsgid/__init__.py
+++ wsgid/__init__.py
@@ -3,4 +3,3 @@
 __progname__ = "wsgid"
 __version__ = "v0.5.0"
 __description__ = 'A complete WSGI environment for mongrel2 handlers'
-
--- wsgid/test.py
+++ wsgid/test.py
@@ -2,21 +2,21 @@
 
 import os
 
+
 class FakeOptions(object):
-  def __init__(self, **kwargs):
-    for k, v in kwargs.items():
-      setattr(self, k, v)
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v)
 
-'''
- Returns the full absolute path
- of the path received.
 
- Useful when you want to know the full
- path of the current file, eg:
-
- full_path(__file__)
-'''
 def fullpath(path):
-  return os.path.dirname(os.path.abspath(os.path.expanduser(path)))
+    '''
+    Returns the full absolute path
+    of the path received.
 
+    Useful when you want to know the full
+    path of the current file, eg:
 
+    full_path(__file__)
+    '''
+    return os.path.dirname(os.path.abspath(os.path.expanduser(path)))
